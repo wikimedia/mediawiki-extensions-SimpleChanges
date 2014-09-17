b03516b165dacc4fbdb857cf0e63df3325f25dc8
Ike Hecht <tosfos@yahoo.com>
2014-09-16 21:42:59 -0700

    change global name, fix whitespace, rename alias file, modernize
    Change-Id: Ie3651ad9d7c5f3c4c837575ca8087e9b2c98e731

--- SpecialSimpleChanges.php
+++ SpecialSimpleChanges.php
@@ -1,7 +1,6 @@
 <?php
 
 class SpecialSimpleChanges extends SpecialRecentChanges {
-
 	public function __construct( $name = 'SimpleChanges' ) {
 		parent::__construct( $name );
 	}
@@ -13,14 +12,15 @@ class SpecialSimpleChanges extends SpecialRecentChanges {
 	 * @return array
 	 */
 	public function buildMainQueryConds( FormOptions $opts ) {
-		global $wgContentNamespaces, $scOnlyContentNamespaces;
+		global $wgContentNamespaces, $wgSimpleChangesOnlyContentNamespaces;
 
 		$conds = parent::buildMainQueryConds( $opts );
 
 		# don't count log entries toward limit of number of changes displayed
 		$conds[] = 'rc_type != ' . RC_LOG;
 
-		if( $opts['namespace'] == '' && $scOnlyContentNamespaces && $wgContentNamespaces != null ) {
+		if ( $opts['namespace'] == '' && $wgSimpleChangesOnlyContentNamespaces &&
+			$wgContentNamespaces != null ) {
 			$contentNamespaces = $wgContentNamespaces;
 
 			$condition = '(rc_namespace = ' . array_shift( $contentNamespaces );
@@ -31,39 +31,38 @@ class SpecialSimpleChanges extends SpecialRecentChanges {
 			$conds[] = $condition;
 		}
 
-		return  $conds;
+		return $conds;
 	}
 
 	/**
 	 * Creates the choose namespace selection
 	 *
-	 * @todo Uses radio buttons (HASHAR)
 	 * @param FormOptions $opts
 	 * @return string
 	 */
 	protected function namespaceFilterForm( FormOptions $opts ) {
-		global $scOnlyContentNamespaces;
+		global $wgSimpleChangesOnlyContentNamespaces;
 
-		if ( !$scOnlyContentNamespaces ) {
+		if ( !$wgSimpleChangesOnlyContentNamespaces ) {
 			return parent::namespaceFilterForm( $opts );
 		}
-		$nonContentNamespaces = array_diff ( MWNamespace::getValidNamespaces(), MWNamespace::getContentNamespaces() );
-		//borrowed from parent class
-		//If scOnlyContentNamespaces is true, we need to change the namespace selector to only show content namespaces.
+		$nonContentNamespaces = array_diff( MWNamespace::getValidNamespaces(),
+			MWNamespace::getContentNamespaces() );
+		//Borrowed from parent class.
+		//If $wgSimpleChangesOnlyContentNamespaces is true, we need to change the namespace
+		//selector to only show content namespaces.
 		$nsSelect = Html::namespaceSelector(
-			array( 'selected' => $opts['namespace'], 'all' => '', 'exclude' => $nonContentNamespaces ),
-			array( 'name' => 'namespace', 'id' => 'namespace' )
+				array( 'selected' => $opts['namespace'], 'all' => '', 'exclude' => $nonContentNamespaces ),
+				array( 'name' => 'namespace', 'id' => 'namespace' )
 		);
 		$nsLabel = Xml::label( $this->msg( 'simplechanges-contentnamespace' )->text(), 'namespace' );
 		$invert = Xml::checkLabel(
-			$this->msg( 'invert' )->text(), 'invert', 'nsinvert',
-			$opts['invert'],
-			array( 'title' => $this->msg( 'tooltip-invert' )->text() )
+				$this->msg( 'invert' )->text(), 'invert', 'nsinvert', $opts['invert'],
+				array( 'title' => $this->msg( 'tooltip-invert' )->text() )
 		);
 		$associated = Xml::checkLabel(
-			$this->msg( 'namespace_association' )->text(), 'associated', 'nsassociated',
-			$opts['associated'],
-			array( 'title' => $this->msg( 'tooltip-namespace_association' )->text() )
+				$this->msg( 'namespace_association' )->text(), 'associated', 'nsassociated',
+				$opts['associated'], array( 'title' => $this->msg( 'tooltip-namespace_association' )->text() )
 		);
 
 		return array( $nsLabel, "$nsSelect $invert $associated" );
@@ -71,52 +70,46 @@ class SpecialSimpleChanges extends SpecialRecentChanges {
 
 	/**
 	 * Send output to the OutputPage object, only called if not used feeds
-	 * This function is a modified combination of SpecialRecentchanges::webOutput() (the parent) &
+	 * This function is a modified combination of SpecialRecentchanges::outputChangesList() &
 	 * ChangesList::recentChangesLine()
 	 *
 	 * @param $rows Array of database rows
 	 * @param $opts FormOptions
 	 */
-	public function webOutput( $rows, $opts ) {
+	public function outputChangesList( $rows, $opts ) {
 		$limit = $opts['limit'];
-
-		if( !$this->including() ) {
-			// Output options box - the legend will use message 'recentchanges-legend' which is not ideal.
-			$this->doHeader( $opts );
-		}
 
 		$counter = 1;
 		$list = ChangesList::newFromContext( $this->getContext() );
 
-		$s = $list->beginRecentChangesList();
+		$rclistOutput = $list->beginRecentChangesList();
 
-		$s .= "\n<ul class=\"special\">\n";
-		foreach( $rows as $obj ) {
-			if( $limit == 0 ) {
+		$rclistOutput .= "\n<ul class=\"special\">\n";
+		foreach ( $rows as $obj ) {
+			if ( $limit == 0 ) {
 				break;
 			}
 			$rc = RecentChange::newFromRow( $obj );
 			$rc->counter = $counter++;
 
-			$classes = array();
 			$changeLine = false;
 
-			if( $rc->mAttribs['rc_log_type'] ) {
-			// Log entries (old format) or log targets, and special pages
-			} elseif( $rc->mAttribs['rc_namespace'] == NS_SPECIAL ) {
-			// Regular entries
+			if ( $rc->mAttribs['rc_log_type'] ) {
+				// Log entries (old format) or log targets, and special pages
+			} elseif ( $rc->mAttribs['rc_namespace'] == NS_SPECIAL ) {
+				// Regular entries
 			} else {
 				$list->insertArticleLink( $changeLine, $rc, false, false );
-				$changeLine = "<li class=\"" . implode( ' ', $classes ) . "\">" . $changeLine . "</li>\n";
+				$changeLine = "<li>" . $changeLine . "</li>\n";
 			}
 
 			if ( $changeLine !== false ) {
-				$s .= $changeLine;
+				$rclistOutput .= $changeLine;
 				--$limit;
 			}
 		}
-		$s .= "\n</ul>\n";
-		$s .= $list->endRecentChangesList();
-		$this->getOutput()->addHTML( $s );
+		$rclistOutput .= "\n</ul>\n";
+		$rclistOutput .= $list->endRecentChangesList();
+		$this->getOutput()->addHTML( $rclistOutput );
 	}
 }
